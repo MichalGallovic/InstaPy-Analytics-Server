7adc31d7a67faebffb4fb8b794c3deb499501f20
MichalGallovic <gallovicm@gmail.com>
2019-03-24 21:02:03 +0700

    Add fillable name to Profile

--- app/Profile.php
+++ app/Profile.php
@@ -6,6 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Profile extends Model
 {
+    /** @var array */
+    protected $fillable = [
+        'name'
+    ];
+
     public function profileActivity()
     {
         return $this->hasMany(ProfileActivity::class);
