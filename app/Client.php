0092825576fb8e041aa794ea0fd0702b726a3cbb
MichalGallovic <gallovicm@gmail.com>
2019-03-24 21:02:58 +0700

    Make Client authenticable
    This is kind of hacky, because clients table does not have
    remember token. Will check later if anything has to be done about this.

--- app/Client.php
+++ app/Client.php
@@ -2,10 +2,13 @@
 
 namespace App;
 
+use Illuminate\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Model;
 
 class Client extends Model
 {
+    use Authenticatable;
+
     /** @var array */
     protected $fillable = [
         'name', 'api_token'
