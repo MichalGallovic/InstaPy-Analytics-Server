74e166ad0cb8d3f6439aac98077910f39fa12396
MichalGallovic <gallovicm@gmail.com>
2019-03-24 18:57:55 +0700

    Add sqlite_testing as in-memory sqlite db for phpunit

--- config/database.php
+++ config/database.php
@@ -39,7 +39,11 @@ return [
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
         ],
-
+        'sqlite_testing' => [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => ''
+        ],
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
